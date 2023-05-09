c5d0bb97c3d1b4d361fe9284a48852f5622a8c67
Anton <anton.stallboerger@hfg-gmuend.de>
2023-05-09 11:14:56 +0200

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import clsx from 'clsx'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +15,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html 
+      lang="en"
+      className={clsx(
+        'text-black bg-white dark:text-white dark:bg-black',
+
+      )}
+    >
+      <body className=''>
+        {/* <Navbar /> */}
+        <main className=''>
+          {children}
+        </main>
+        {/* <Footer /> */}
+      </body>
     </html>
   )
 }
