de9d48a326aa7add4755ca2efee9e84edaf39879
Anton <anton.stallboerger@hfg-gmuend.de>
2023-05-09 11:36:20 +0200

    verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,59 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import clsx from 'clsx'
+import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Anton Stallbörger',
-  description: 'Digital Product Designer and Developer driven by curiosity',
-}
+export const metadata: Metadata = {
+  title: {
+    default: 'Anton Stallbörger',
+    template: '%s | Anton Stallbörger',
+  },
+  description: 'Digital Product Designer and Developer.',
+  openGraph: {
+    title: 'Anton Stallbörger',
+    description: 'Digital Product Designer and Developer.',
+    url: 'https://antonstallboerger.com',
+    siteName: 'Anton Stallbörger',
+    images: [
+      {
+        url: 'https://leerob.io/og.jpg',
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: 'en-US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    title: 'Anton Stallbörger',
+    card: 'summary_large_image',
+  },
+  icons: {
+    shortcut: '/favicon.ico',
+  },
+  verification: {
+    google: 'google-site-verification=sD6u1ZwlQ5H2dnzBEhKvZ_uHxFIgaOd5P63k-_0AA7M',
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html 
@@ -26,6 +67,7 @@ export default function RootLayout({
         {/* <Navbar /> */}
         <main className=''>
           {children}
+          <Analytics />
         </main>
         {/* <Footer /> */}
       </body>
