ad15f64b4ba0de37d897c8f62abec54165635966
Linus Rogge <hello@linusrogge.com>
2023-08-06 19:14:24 +0200

    (add) active link style

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,22 +2,27 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
+    const currentRoute = usePathname();
+
+    const defaultLinkStyle = "hover:underline"
+    const activeLinkStyle = " text-black dark:text-white"
 
     return (
         <nav className='fixed grid-cols-16 grid gap-4 md:gap-8 justify-between items-center w-full h-[53px] z-50 px-4 lg:px-8 border-b border-zinc-200 bg-white dark:border-zinc-800 dark:bg-black select-none'>
-            <Link href='/' className='hover:underline'>
+            <Link href='/' className={currentRoute === "/" ? activeLinkStyle : defaultLinkStyle}>
                 Home
             </Link>
             <div className='gap-6 flex col-span-5 col-start-9'>
-                <Link href='/about' className='hover:underline'>
+                <Link href='/about' className={currentRoute === "/about" ? activeLinkStyle : defaultLinkStyle}>
                     About
                 </Link>
-                <Link href='/thoughts' className='hover:underline'>
+                <Link href='/thoughts' className={currentRoute === "/thoughts" ? activeLinkStyle : defaultLinkStyle}>
                     Thoughts
                 </Link>
-                <Link href='/playground' className='hover:underline'>
+                <Link href='/playground' className={currentRoute === "/playground" ? activeLinkStyle : defaultLinkStyle}>
                     Playground
                 </Link>
             </div>
