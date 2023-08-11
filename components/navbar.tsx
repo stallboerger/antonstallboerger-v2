f9e8a304bdf8612b4c5dee8eec4f1ae21cd732ee
Anton <anton.stallboerger@hfg-gmuend.de>
2023-08-12 01:35:37 +0200

    (outsource) navbar styles

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,33 +4,31 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
+import styles from '../styles/components/navbar.module.scss'
+
 export default function Navbar() {
     const currentRoute = usePathname();
 
-    const defaultLinkStyle = "hover:underline"
-    const activeLinkStyle = " text-black dark:text-white"
-
     return (
-        <nav className='fixed grid-cols-16 grid gap-4 md:gap-8 justify-between items-center w-full h-[53px] z-50 px-4 lg:px-8 border-b border-zinc-200 bg-white dark:border-zinc-800 dark:bg-black select-none'>
-            <div className='col-span-3'>
-                <Link href='/' className={currentRoute === "/" ? activeLinkStyle : defaultLinkStyle}>
-                    Home
-                </Link>
-            </div>
-            <div className='gap-3 md:gap-6 flex md:col-span-5 col-start-[16] justify-self-end md:justify-self-start md:col-start-9'>
-                <Link href='/about' className={currentRoute === "/about" ? activeLinkStyle : defaultLinkStyle}>
+        <nav className={styles.navbar}>
+            <Link href='/' className={`${styles.home} ${styles.link} ${currentRoute === "/" && styles.active}`}>
+                Home
+            </Link>
+
+            <div className={styles.pages}>
+                <Link href='/about' className={`${styles.link} ${currentRoute === "/about" && styles.active}`}>
                     About
                 </Link>
-                <Link href='/thoughts' className={currentRoute === "/thoughts" ? activeLinkStyle : defaultLinkStyle}>
+                <Link href='/thoughts' className={`${styles.link} ${currentRoute === "/thoughts" && styles.active}`}>
                     Thoughts
                 </Link>
-                <Link href='/playground' className={currentRoute === "/playground" ? activeLinkStyle : defaultLinkStyle}>
+                <Link href='/playground' className={`${styles.link} ${currentRoute === "/playground" && styles.active}`}>
                     Playground
                 </Link>
             </div>
             <Image 
                 alt='Logo · Anton Stallbörger'
-                className='hover:animate-spin-slow h-4 w-4 self-center dark:invert col-start-[16] justify-self-end hidden md:block'
+                className={styles.logo}
                 src='/logo.svg'
                 width={24}
                 height={24}
