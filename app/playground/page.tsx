e2245e7ea6c23b524bbb05eeabd0bbc4f3784ad5
Linus Rogge <hello@linusrogge.com>
2023-08-11 22:59:44 +0200

    various changes

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -4,51 +4,38 @@ import Link from 'next/link'
 import Card from '@/components/card'
 
 import styles from '../../styles/views/playground.module.scss'
+import { allProjects } from 'contentlayer/generated';
+import { compareDesc, format, parseISO } from 'date-fns';
+
 export const metadata: Metadata = {
-  title: 'Playground',
-  description: 'Software Designer',
+	title: 'Playground',
+	description: 'Software Designer',
 };
 
+const filteredProjects = allProjects.filter((project) => project.playground)
+const projects = filteredProjects.sort((a, b) => 
+	compareDesc(new Date(a.year), new Date(b.year))
+)
 
 export default function Playground() {
   return (
     <>
-      <section className='col-span-full md:col-span-12 lg:col-span-5 lg:sticky lg:top-[calc(53px_+_32px)] lg:self-start mb-20 lg:mb-0'>
-        <p className='mb-8'>
-          <span className='font-bold'>Playground</span>
-          <br />
-          Software Designer
-        </p>
-        <p className=''>
-          Besides my  
-        </p>  
-      </section>
+		<header className={styles.header}>
+			<h1 className={styles.title}>Playground</h1>
+			<p className={styles.count}>{projects.length} concepts</p>
+		</header>
 
-      <section className='col-span-full lg:col-start-9 lg:col-span-8'>
-        <Card
-          title='Great Apps'
-          imageSrc='/images/main/boost.png'
-          imageAlt='Great Apps · Anton Stallbörger'
-          description='Apps with that special something. A collection of apps that are unique, innovative, and beautifully designed.'
-          year='2023'
-          link='/boost'
-        />
-        <Card 
-          title='Great Finds'
-          imageSrc='/images/main/curations.png'
-          imageAlt='Great Finds · Anton Stallbörger'
-          description='A curated collection of stunning tools and website inspirations for free. Helping designers, developers, and everyone else discover new and innovative tools and ideas for their projects.'
-          year='2022 &ndash; Present'
-          link='/curations'
-        />
-        <Card
-          title='UPS App'
-          imageSrc='/images/main/ups.png'
-          imageAlt='UPS App · Anton Stallbörger'
-          description='A redesign concept of the UPS App for a faster and easier way to send and track your packages.'
-          year='2022'
-          link='/ups'
-        />
+		<section className={styles.projects}>
+			{projects.map((project, idx) => (
+				<Card
+					title={project.title}
+					imageSrc={`/images/projects/${project.slug}/${project.image}`}
+					imageAlt={`${project.title} · Anton Stallbörger`}
+					description={project.description}
+					year={format(parseISO(project.year), 'yyyy')}
+					link={`/projects/${project.slug}`}
+				/>
+			))}
       </section>
     </>
   )
