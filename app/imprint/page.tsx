fde3f25b71b95bf1eabf83c516635724eb53ef16
Anton <anton.stallboerger@hfg-gmuend.de>
2023-08-12 00:40:04 +0200

    (update) content

--- app/imprint/page.tsx
+++ app/imprint/page.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 
+import styles from '../../styles/views/imprint.module.scss'
+
 export const metadata: Metadata = {
   title: 'Imprint',
   description: 'Digital Product Designer and Developer.',
@@ -8,33 +10,33 @@ export const metadata: Metadata = {
 export default function ImprintPage() {
   return (
     <>
-      <section className='col-span-full md:col-span-8 lg:top-[calc(53px_+_32px)] lg:self-start'>
-        <h1 className=''>Imprint</h1>
-        <p className='mb-20'>Last updated Aug 11, 2023</p>
-      </section>
-      <section className='md:col-span-6'>
-        <h2 className='text-base mb-4 font-bold'>Information according to German §5 of TMG</h2>
-        <p className='mb-20'>
+      <header className={styles.header}>
+        <h1>Imprint</h1>
+        <p>Last updated Aug 11, 2023</p>
+      </header>
+      <section className={styles.content}>
+        <h2>Information according to German §5 of TMG</h2>
+        <p>
           Anton Stallbörger
           <br />
           Eduard-Pfeiffer-Str. 97
           <br />
           70192 Stuttgart, Germany
           <br />
-          <a href="mailto:anton@stallboerger.de" className='text-zinc-500 hover:underline'>anton@stallboerger.de</a>
+          <a href="mailto:anton@stallboerger.de" >anton@stallboerger.de</a>
         </p>
-        <h2 className='text-base mb-4 font-bold'>Copyright</h2>
-        <p className='mb-20'>
+        <h2>Copyright</h2>
+        <p>
           This website and its contents are subject to German copyright law. Unless expressly permitted by law (§ 44a et seq. of the copyright law), every form of utilizing, reproducing or processing works subject to copyright protection on this website requires my prior consents.
-          <br />
-          <br />
+        </p>
+        <p>
           Individual reproductions of a work are allowed only for private use, so must not serve either directly or indirectly for earnings. Unauthorized utilization of copyrighted works is punishable (§ 106 of the copyright law).
         </p>
-        <h2 className='text-base mb-4 font-bold'>Limitation of liability for internal content</h2>
+        <h2>Limitation of liability for internal content</h2>
         <p>      
           The contents of this website have been created with my utmost care. However, I cannot guarantee the contents&rsquo; accuracy, completeness or topicality. According to statutory provisions, I&rsquo;m furthermore responsible for my own content. In this context, please note that I&rsquo;m accordingly not obliged to monitor merely the transmitted or saved information of third parties, or investigate circumstances pointing to illegal activity.
-          <br />
-          <br />
+        </p>
+        <p>
           My obligation to remove or block the use of information under generally applicable laws remain unaffected by this as per §§ 8 to 10 of the Telemedia Act (TMG).
         </p>
       </section>
