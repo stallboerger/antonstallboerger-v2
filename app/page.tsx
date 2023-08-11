e4efafcb8f47458ac3d82c4debb41a3d5e3d8522
Linus Rogge <hello@linusrogge.com>
2023-08-11 22:53:58 +0200

    (move) Card component to own file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
 import Link from 'next/link'
+import Card from '@/components/card'
 
 import { allProjects } from 'contentlayer/generated'
 import { format, parseISO, compareDesc } from 'date-fns'
@@ -8,54 +8,6 @@ const filteredProjects = allProjects.filter((project) => !project.playground)
 const projects = filteredProjects.sort((a, b) =>
   compareDesc(new Date(a.year), new Date(b.year))
 )
-
-interface Props {
-  title: string;
-  imageSrc: string;
-  imageAlt: string;
-  description: string;
-  year: string;
-  link: string;
-}
-
-const Card: React.FC<Props> = ({
-  title,
-  imageSrc,
-  imageAlt,
-  description,
-  year,
-  link,
-}) => {
-  return (
-    <div className='mb-24 [&:last-child]:mb-0 [&:last-child>div:last-child]:mb-0 [&:last-child>div:last-child>p]:mb-0 border-b border-zinc-200 dark:border-zinc-800 [&:last-child]:border-transparent'>
-      <p className='mb-8 sticky top-[53px] bg-gradient-to-b from-white via-white/90  dark:from-black dark:via-black/90 to-transparent z-10 pb-3 pt-8 mt-[-32px]'>
-        <span className='font-bold'>{title}</span>
-        <br />
-        {year}
-      </p>
-      <div className='mb-8 dark:bg-zinc-900 bg-zinc-100'>
-        <Link href={link}>
-          <Image
-            alt={imageAlt}
-            className='hover:scale-105 transform transition-all duration-300 select-none'
-            src={imageSrc}
-            width={3840}
-            height={3840}
-            priority
-          />
-        </Link>
-      </div>
-      <div className='mb-8 grid grid-cols-8 gap-8'>
-        <p className='col-span-full sm:col-span-6'>
-          {description}
-        </p>
-        <Link href={link} className='group hover:underline text-zinc-500 col-span-full sm:col-span-2 sm:justify-self-end'>
-          Learn more
-        </Link>
-      </div>
-    </div>
-  );
-};
 
 export default function Home() {
   return (
--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,59 +1,13 @@
 import type { Metadata } from 'next';
 import Image from 'next/image'
 import Link from 'next/link'
+import Card from '@/components/card'
 
 export const metadata: Metadata = {
   title: 'Playground',
   description: 'Software Designer',
 };
 
-interface Props {
-  title: string;
-  imageSrc: string;
-  imageAlt: string;
-  description: string;
-  year: string;
-  link: string;
-}
-
-const Card: React.FC<Props> = ({
-  title,
-  imageSrc,
-  imageAlt,
-  description,
-  year,
-  link,
-}) => {
-  return (
-    <div className='mb-24 [&:last-child]:mb-0 [&:last-child>div:last-child]:mb-0 [&:last-child>div:last-child>p]:mb-0 border-b border-zinc-200 dark:border-zinc-800 [&:last-child]:border-transparent'>
-      <p className='mb-8'>
-        <span className='font-bold'>{title}</span>
-        <br />
-        {year}
-      </p>
-      <div className='mb-8 dark:bg-zinc-900 bg-zinc-100'>
-        <Link href={link}>
-          <Image
-            alt={imageAlt}
-            className='hover:scale-105 transform transition-all duration-300 select-none'
-            src={imageSrc}
-            width={3840}
-            height={3840}
-            priority
-          />
-        </Link>
-      </div>
-      <div className='mb-24 grid grid-cols-8 gap-8'>
-        <p className='col-span-full sm:col-span-6'>
-          {description}
-        </p>
-        <Link href={link} className='group hover:underline text-zinc-500 col-span-full sm:col-span-2 sm:justify-self-end'>
-          Learn more
-        </Link>
-      </div>
-    </div>
-  );
-};
 
 export default function Playground() {
   return (
