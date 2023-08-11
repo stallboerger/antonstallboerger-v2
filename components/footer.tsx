3603ba875e23efe61ca80d75b8590a3254731b34
Anton <anton.stallboerger@hfg-gmuend.de>
2023-08-12 01:24:20 +0200

    (outsource) footer styles

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,92 +3,109 @@
 import dynamic from 'next/dynamic'
 import Link from 'next/link';
 
+import styles from '../styles/components/footer.module.scss'
+
 const Clock = dynamic(() => import('./clock'), { ssr: false })
 
 export default function Footer() {
     return (
-        <footer className='mt-40 grid-cols-8 sm:grid-cols-16 grid gap-6 md:gap-8 px-4 md:px-8 mb-8 text-sm'>
-            <section className='grid grid-cols-8 gap-6 col-span-full lg:col-span-8 row-start-2 lg:row-auto self-start border-t border-zinc-200 pt-8 dark:border-zinc-800'>
-                <div className="flex flex-col col-span-2 gap-2">
-                    <p className="font-bold text-zinc-900 dark:text-zinc-200">
-                        Connect
-                    </p>
-                    <a 
-                        href="https://twitter.com/stallboerger" 
-                        className="hover:text-blue-600 self-start" 
-                        target="_blank" 
-                        rel="noreferrer"
-                    >
-                        Twitter
-                    </a>
-                    <a 
-                        href="imessage://anton@stallboerger.de" 
-                        className='hover:text-green-600 self-start'
-                        target="_blank" 
-                        rel="noreferrer"
-                    >
-                        iMessage
-                    </a>
-                    <a 
-                        href="https://www.cosmos.so/stallboerger" 
-                        className='self-start' 
-                        target="_blank" 
-                        rel="noreferrer"
-                    >
-                        Cosmos
-                    </a>
-                    <a 
-                        href="https://read.cv/stallboerger" 
-                        className='self-start' 
-                        target="_blank" 
-                        rel="noreferrer"
-                    >
-                        Read.cv
-                    </a>
+        <footer className={styles.footer}>
+            <section className={styles.links}>
+                <div className={styles.connect}>
+                    <h2 className={styles.title}>Connect</h2>
+                    <ul className={styles.list}>
+                        <li>
+                            <a 
+                                href="https://twitter.com/stallboerger" 
+                                className={`${styles.link} ${styles.twitter}`}
+                                target="_blank" 
+                                rel="noreferrer"
+                            >
+                                Twitter
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                                href="imessage://anton@stallboerger.de" 
+                                className={`${styles.link} ${styles.imessage}`}
+                                target="_blank" 
+                                rel="noreferrer"
+                            >
+                                iMessage
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                                href="https://www.cosmos.so/stallboerger" 
+                                className={styles.link} 
+                                target="_blank" 
+                                rel="noreferrer"
+                            >
+                                Cosmos
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                                href="https://read.cv/stallboerger" 
+                                className={`${styles.link} ${styles.readcv}`}
+                                target="_blank" 
+                                rel="noreferrer"
+                            >
+                                Read.cv
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-                <div className="flex flex-col col-span-4 lg:col-span-4 col-start-8 lg:col-start-5 gap-2">
-                    <p className="text-sm font-bold text-zinc-900 dark:text-zinc-200">
-                        Explore
-                    </p>
-                    <Link 
-                        href='/about' 
-                        className='self-start'
-                    >
-                        About
-                    </Link>
-                    <Link 
-                        href='/thoughts' 
-                        className='self-start'
-                    >
-                        Thoughts
-                    </Link>
-                    <Link 
-                        href='/playground' 
-                        className='self-start'
-                    >
-                        Playground
-                    </Link>
-                </div>                     
-                <p className="text-sm text-zinc-900 dark:text-zinc-200 font-bold lg:col-span-3 col-span-full md:col-span-2">&copy; 2023 Anton Stallbörger</p>
-                <div className='flex flex-row gap-4 col-start-0 md:col-start-3 lg:col-start-5'>
+
+                <div className={styles.explore}>
+                    <h2 className={styles.title}>Explore</h2>
+                    <ul className={styles.list}>
+                        <li>
+                            <Link 
+                                href='/about' 
+                                className={styles.link}
+                            >
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link 
+                                href='/thoughts' 
+                                className={styles.link}
+                            >
+                                Thoughts
+                            </Link>
+                        </li>
+                        <li>
+                            <Link 
+                                href='/playground' 
+                                className='self-start'
+                            >
+                                Playground
+                            </Link>
+                        </li>
+                    </ul>
+                </div>   
+
+                <p className={styles.copyright}>&copy; 2023 Anton Stallbörger</p>
+                <div className={styles.legals}>
                     <Link 
                         href='/colophon' 
-                        className=''
+                        className={styles.link}
                     >
                         Colophon
                     </Link>
                     <Link 
                         href='/imprint' 
-                        className=''
+                        className={styles.link}
                     >
                         Imprint
                     </Link> 
                 </div>
             </section>
-            <section className='col-span-full lg:col-start-9 lg:col-span-8 row-start-1 lg:row-auto self-start'> 
-                <div className='flex flex-col justfy-between lg:border-t pt-6 border-zinc-200 dark:border-zinc-800'> 
-                    <Clock />       
-                </div>
+
+            <section className={styles.clock}> 
+                <Clock />
             </section>
         </footer>
     );
