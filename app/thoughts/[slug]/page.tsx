ad5b988301412377635296ff7316a4a6cd7a73a4
Linus Rogge <hello@linusrogge.com>
2023-08-11 03:05:31 +0200

    (fix) might be undefined

--- app/thoughts/[slug]/page.tsx
+++ app/thoughts/[slug]/page.tsx
@@ -4,31 +4,44 @@ import Image from 'next/image'
 
 export const generateStaticParams = async () => allPosts.map((post) => ({ slug: post._raw.flattenedPath }))
 
-export const generateMetadata = ({ params }) => {
-  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
-  return { title: post?.title }
+type Params = {
+    title: string;
+    slug: string;
+    date: Date;
+    image: string;
+}
+
+export const generateMetadata = (params: Params) => {
+    const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
+    return { title: post?.title }
 }
 
 const PostLayout = ({ params }: { params: { slug: string } }) => {
     const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
 
-    return (
-        <>
-            <Image 
-                src={`/images/thoughts/${post.image}`}
-                alt={post.title}
-                width="1920" 
-                height="305"
-                className='mt-[-140px] mb-2 col-span-full w-[calc(100%_+_(2_*_2rem))] translate-x-[-2rem] max-w-none h-[70vh] object-cover' />
-            <header className='col-span-full md:col-span-12 lg:col-span-8'>
-                <h1 className=''>{post?.title}</h1>
-                <time dateTime={post?.date} className="text-zinc-500">
-                    {format(parseISO(post.date), 'eee · LLLL d, yyyy')}
-                </time>
-            </header>
-            <article className='col-span-full lg:col-start-9 lg:col-span-6' dangerouslySetInnerHTML={{ __html: post?.body.html }}></article>
-        </>
-    )
+    if (post) {
+        return (
+            <>
+                <Image 
+                    src={`/images/thoughts/${post?.image}`}
+                    alt={post.title}
+                    width="1920" 
+                    height="305"
+                    className='mt-[-140px] mb-2 col-span-full w-[calc(100%_+_(2_*_2rem))] translate-x-[-2rem] max-w-none h-[70vh] object-cover' />
+                <header className='col-span-full md:col-span-12 lg:col-span-8'>
+                    <h1 className=''>{post?.title}</h1>
+                    <time dateTime={post?.date} className="text-zinc-500">
+                        {format(parseISO(post.date), 'eee · LLLL d, yyyy')}
+                    </time>
+                </header>
+                <article className='col-span-full lg:col-start-9 lg:col-span-6' dangerouslySetInnerHTML={{ __html: post?.body.html }}></article>
+            </>
+        )
+    } else {
+        return (
+            <h1>Thought could not be found</h1>
+        )
+    }
 }
 
 export default PostLayout
