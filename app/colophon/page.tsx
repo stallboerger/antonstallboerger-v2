eca4efcfc0b36e8bbcbb190e298783446d8014d8
Anton <anton.stallboerger@hfg-gmuend.de>
2023-08-12 00:39:54 +0200

    (update) content

--- app/colophon/page.tsx
+++ app/colophon/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import Image from 'next/image';
 
+import styles from '../../styles/views/colophon.module.scss'
+
 export const metadata: Metadata = {
   title: 'Colophon',
   description: 'Digital Product Designer and Developer.',
@@ -9,113 +11,135 @@ export const metadata: Metadata = {
 export default function ColophonPage() {
   return (
     <>
-        <section className='col-span-full lg:col-span-8 md:col-span-12 lg:top-[calc(53px_+_32px)] lg:self-start'>
-            <h1 className=''>Colophon</h1>
-            <p className='mb-20'>Last updated Aug 11, 2023</p>
-        </section>
-        <section className='lg:col-span-6'>
-            <h2 className='mb-4 text-base'>Typography</h2>
-            <p className='mb-20 leading-relaxed'>
+        <header className={styles.header}>
+            <h1 className={styles.title}>Colophon</h1>
+            <p className={styles.date}>Last updated Aug 11, 2023</p>
+        </header>
+
+        <section className={styles.content}>
+            <h2>Typography</h2>
+            <p>
                 <a href='https://klim.co.nz/retail-fonts/soehne/' className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">Söhne</a> is a typeface designed and published by the <a href='https://klim.co.nz/' className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">Klim Type Foundry</a>. It is the memory of Akzidenz-Grotesk framed through the reality of Helvetica. Söhne captures the analogue materiality of “Standard Medium” used in Unimark&rsquo;s legendary wayfinding system for the NYC Subway.
             </p>
-            <h2 className='mb-4 text-base'>Tech stack</h2>
-            <p className='mb-6 leading-relaxed'>
-                Built with <a href='https://nextjs.org/' className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">Next.js</a> and <a href='https://tailwindcss.com/' className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">Tailwind CSS</a>. Hosted on <a href='https://vercel.com/home' className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">Vercel</a>. Privacy friendly analytics with <a href='https://vercel.com/analytics' className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">Vercel Analytics</a>.
+
+            <h2>Tech stack</h2>
+            <p>
+                Built with <a href='https://nextjs.org/' target="_blank" rel="noreferrer">Next.js</a> and <a href='https://tailwindcss.com/' target="_blank" rel="noreferrer">Tailwind CSS</a>. Hosted on <a href='https://vercel.com/home' target="_blank" rel="noreferrer">Vercel</a>. Privacy friendly analytics with <a href='https://vercel.com/analytics' target="_blank" rel="noreferrer">Vercel Analytics</a>.
             </p>    
-            <a href="https://github.com/antonstallboerger/antonstallboerger-v2" className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">GitHub Repository</a>
+            <p>
+                <a href="https://github.com/antonstallboerger/antonstallboerger-v2" className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">GitHub Repository</a>
+            </p>
             
-            <h2 className='mb-4 mt-20 text-base'>Photography</h2>
-            <p className='mb-20 leading-relaxed'>
+            <h2>Photography</h2>
+            <p>
                 Images were taken or created by Anton Stallbörger, if not stated otherwise. 
             </p>
 
-            <h2 className='mb-4 mt-20 text-base'>Mockups</h2>
-            <p className='mb-20 leading-relaxed'>
+            <h2>Mockups</h2>
+            <p>
                 All mockups are by  <a href='https://shots.so/' className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">Shots</a> or <a href='https://supply.family/' className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">Supply Family</a>. 
             </p>
 
-            <h2 className='mb-4 mt-6 text-base'>Inspiration</h2>
-            <div className='flex flex-col gap-2 mb-20'>
-                <a 
-                    href="https://linusrogge.com/" 
-                    className='text-zinc-500 w-fit hover:underline' 
-                    target="_blank" 
-                    rel="noreferrer"
-                >
-                    Linus Rogge
-                </a>
-                <a 
-                    href="https://sdrn.co/" 
-                    className='text-zinc-500 w-fit hover:underline' 
-                    target="_blank" 
-                    rel="noreferrer"
-                >
-                    Siddharth Arun
-                </a>
-                <a 
-                    href="https://www.wireframe.co/" 
-                    className='text-zinc-500 w-fit hover:underline' 
-                    target="_blank" 
-                    rel="noreferrer"
-                >
-                    Wireframe
-                </a>
-                <a 
-                    href="https://www.studiolenzing.com/" 
-                    className='text-zinc-500 w-fit hover:underline' 
-                    target="_blank" 
-                    rel="noreferrer"
-                >
-                    Studio Lenzing
-                </a>
-                <a 
-                    href="https://goods.wtf/" 
-                    className='text-zinc-500 w-fit hover:underline' 
-                    target="_blank" 
-                    rel="noreferrer"
-                >
-                    Goods
-                </a>
-                <a 
-                    href="https://ollyevans.xyz/" 
-                    className='text-zinc-500 w-fit hover:underline' 
-                    target="_blank" 
-                    rel="noreferrer"
-                >
-                    Olly Evans
-                </a>
-            </div>
+            <h2>Inspiration</h2>
+            <ul>
+                <li>
+                    <a 
+                        href="https://linusrogge.com/" 
+                        className='text-zinc-500 w-fit hover:underline' 
+                        target="_blank" 
+                        rel="noreferrer"
+                    >
+                        Linus Rogge
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        href="https://sdrn.co/" 
+                        className='text-zinc-500 w-fit hover:underline' 
+                        target="_blank" 
+                        rel="noreferrer"
+                    >
+                        Siddharth Arun
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        href="https://www.wireframe.co/" 
+                        className='text-zinc-500 w-fit hover:underline' 
+                        target="_blank" 
+                        rel="noreferrer"
+                    >
+                        Wireframe
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        href="https://www.studiolenzing.com/" 
+                        className='text-zinc-500 w-fit hover:underline' 
+                        target="_blank" 
+                        rel="noreferrer"
+                    >
+                        Studio Lenzing
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        href="https://goods.wtf/" 
+                        className='text-zinc-500 w-fit hover:underline' 
+                        target="_blank" 
+                        rel="noreferrer"
+                    >
+                        Goods
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        href="https://ollyevans.xyz/" 
+                        className='text-zinc-500 w-fit hover:underline' 
+                        target="_blank" 
+                        rel="noreferrer"
+                    >
+                        Olly Evans
+                    </a>
+                </li>
+            </ul>
 
-            <h2 className='mb-4 mt-6 text-base'>Great humans</h2>
-            <p className='mb-6 leading-relaxed'>
+            <h2>Great humans</h2>
+            <p>
                 I am especially grateful for the support of the following humans:
             </p>
-            <div className='flex flex-col gap-2'>
-                <a 
-                    href="https://linusrogge.com/" 
-                    className='text-zinc-500 w-fit hover:underline' 
-                    target="_blank" 
-                    rel="noreferrer"
-                >
-                    Linus Rogge
-                </a>
-                <a 
-                    href="https://designwithtech.com/" 
-                    className='text-zinc-500 w-fit hover:underline'
-                    target="_blank" 
-                    rel="noreferrer"
-                >
-                    Florian Kiem
-                </a>
-                <a 
-                    href="https://www.nilseller.com/" 
-                    className='text-zinc-500 w-fit hover:underline'
-                    target="_blank" 
-                    rel="noreferrer"
-                >
-                    Nils Eller
-                </a>
-            </div>
+            <ul>
+                <li>
+                    <a 
+                        href="https://linusrogge.com/" 
+                        className='text-zinc-500 w-fit hover:underline' 
+                        target="_blank" 
+                        rel="noreferrer"
+                    >
+                        Linus Rogge
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        href="https://designwithtech.com/" 
+                        className='text-zinc-500 w-fit hover:underline'
+                        target="_blank" 
+                        rel="noreferrer"
+                    >
+                        Florian Kiem
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        href="https://www.nilseller.com/" 
+                        className='text-zinc-500 w-fit hover:underline'
+                        target="_blank" 
+                        rel="noreferrer"
+                    >
+                        Nils Eller
+                    </a>
+                </li>
+            </ul>
         </section>
     </>
   );
