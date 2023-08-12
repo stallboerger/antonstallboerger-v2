2fe7ebf40bd4496235a22cc90517634aa036ce2e
Linus Rogge <hello@linusrogge.com>
2023-08-12 21:48:49 +0200

    (try) swup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import '/styles/base.scss'
+import { useEffect } from 'react'
 import Soehne from 'next/font/local'
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
+
+import Swup from 'swup'
+import SwupHeadPlugin from '@swup/head-plugin'
 
 const soehne = Soehne({
   src: [
@@ -25,76 +31,84 @@ const soehne = Soehne({
   ],
 })
 
-export const metadata: Metadata = {
-  title: {
-    default: 'Anton Stallbörger',
-    template: '%s · Anton Stallbörger',
-  },
-  description: 'Software Designer',
-  openGraph: {
-    title: 'Anton Stallbörger',
-    description: 'Software Designer',
-    url: 'https://antonstallboerger-v2.vercel.app/',
-    siteName: 'Anton Stallbörger',
-    images: [
-      {
-        url: 'https://antonstallboerger-v2.vercel.app/og.jpg',
-        width: 1920,
-        height: 1080,
-      },
-    ],
-    locale: 'en-US',
-    type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  twitter: {
-    title: 'Anton Stallbörger',
-    card: 'summary_large_image',
-    description: 'Software Designer',
-    images: [
-      {
-        url: 'https://antonstallboerger-v2.vercel.app/og.jpg',
-        width: 1920,
-        height: 1080,
-      },
-    ],
-  },
-  icons: {
-    shortcut: '/favicon.ico',
-  },
-  // verification: {
-  //   google: 'google-site-verification=sD6u1ZwlQ5H2dnzBEhKvZ_uHxFIgaOd5P63k-_0AA7M',
-  // },
-};
+// export const metadata: Metadata = {
+//   title: {
+//     default: 'Anton Stallbörger',
+//     template: '%s · Anton Stallbörger',
+//   },
+//   description: 'Software Designer',
+//   openGraph: {
+//     title: 'Anton Stallbörger',
+//     description: 'Software Designer',
+//     url: 'https://antonstallboerger-v2.vercel.app/',
+//     siteName: 'Anton Stallbörger',
+//     images: [
+//       {
+//         url: 'https://antonstallboerger-v2.vercel.app/og.jpg',
+//         width: 1920,
+//         height: 1080,
+//       },
+//     ],
+//     locale: 'en-US',
+//     type: 'website',
+//   },
+//   robots: {
+//     index: true,
+//     follow: true,
+//     googleBot: {
+//       index: true,
+//       follow: true,
+//       'max-video-preview': -1,
+//       'max-image-preview': 'large',
+//       'max-snippet': -1,
+//     },
+//   },
+//   twitter: {
+//     title: 'Anton Stallbörger',
+//     card: 'summary_large_image',
+//     description: 'Software Designer',
+//     images: [
+//       {
+//         url: 'https://antonstallboerger-v2.vercel.app/og.jpg',
+//         width: 1920,
+//         height: 1080,
+//       },
+//     ],
+//   },
+//   icons: {
+//     shortcut: '/favicon.ico',
+//   },
+//   // verification: {
+//   //   google: 'google-site-verification=sD6u1ZwlQ5H2dnzBEhKvZ_uHxFIgaOd5P63k-_0AA7M',
+//   // },
+// };
 
 export default function RootLayout({
-  children,
+  	children,
 }: {
-  children: React.ReactNode;
+  	children: React.ReactNode;
 }) {
-  return (
-    <html 
-      lang="en"
-      className={soehne.className}
-    >
-      <body className='antialiased text-[#343433] bg-white dark:text-[#cbcbcc] dark:bg-black text-base selection:bg-[#D8ECFC] selection:text-[#008CFF] dark:selection:bg-[#0E1E35] min-h-full flex flex-col'>
-        <Navbar />
-        <main className='pt-[140px] md:pt-[180px] h-auto px-4 mb-auto lg:px-8 grid-cols-8 sm:grid-cols-16 grid gap-4 md:gap-8'>
-          {children}
-          <Analytics />
-        </main>
-        <Footer />
-      </body>
-    </html>
-  )
+	useEffect(() => {
+		const swup = new Swup({
+			plugins: [new SwupHeadPlugin({ persistAssets: true, awaitAssets: true })]
+		});
+	})
+
+	return (
+		<html 
+			lang="en"
+			className={soehne.className}
+		>
+			<body className='antialiased text-[#343433] bg-white dark:text-[#cbcbcc] dark:bg-black text-base selection:bg-[#D8ECFC] selection:text-[#008CFF] dark:selection:bg-[#0E1E35] min-h-full flex flex-col'>
+				<Navbar />
+				
+				<main id='swup' className='transition-fade pt-[140px] md:pt-[180px] h-auto px-4 mb-auto lg:px-8 grid-cols-8 sm:grid-cols-16 grid gap-4 md:gap-8'>
+					{children}
+				</main>
+
+				<Analytics />
+				<Footer />
+			</body>
+		</html>
+	)
 }
