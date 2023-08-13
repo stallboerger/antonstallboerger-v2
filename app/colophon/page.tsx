09d9433860d4a6dfdecefb716ebf0b9ca7c3ea3d
Anton <anton.stallboerger@hfg-gmuend.de>
2023-08-13 23:24:32 +0200

    (adjust) links

--- app/colophon/page.tsx
+++ app/colophon/page.tsx
@@ -19,15 +19,15 @@ export default function ColophonPage() {
         <section className={styles.content}>
             <h2>Typography</h2>
             <p>
-                <a href='https://klim.co.nz/retail-fonts/soehne/' className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">Söhne</a> is a typeface designed and published by the <a href='https://klim.co.nz/' className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">Klim Type Foundry</a>. It is the memory of Akzidenz-Grotesk framed through the reality of Helvetica. Söhne captures the analogue materiality of “Standard Medium” used in Unimark&rsquo;s legendary wayfinding system for the NYC Subway.
+                <a href='https://klim.co.nz/retail-fonts/soehne/' className={styles.link} target="_blank" rel="noreferrer">Söhne</a> is a typeface designed and published by the <a href='https://klim.co.nz/' className={styles.link} target="_blank" rel="noreferrer">Klim Type Foundry</a>. It is the memory of Akzidenz-Grotesk framed through the reality of Helvetica. Söhne captures the analogue materiality of “Standard Medium” used in Unimark&rsquo;s legendary wayfinding system for the NYC Subway.
             </p>
 
             <h2>Tech stack</h2>
             <p>
-                Built with <a href='https://nextjs.org/' target="_blank" rel="noreferrer">Next.js</a> and <a href='https://tailwindcss.com/' target="_blank" rel="noreferrer">Tailwind CSS</a>. Hosted on <a href='https://vercel.com/home' target="_blank" rel="noreferrer">Vercel</a>. Privacy friendly analytics with <a href='https://vercel.com/analytics' target="_blank" rel="noreferrer">Vercel Analytics</a>.
+                Built with <a href='https://nextjs.org/' className={styles.link} target="_blank" rel="noreferrer">Next.js</a> and <a href='https://tailwindcss.com/' className={styles.link} target="_blank" rel="noreferrer">Tailwind CSS</a>. Hosted on <a href='https://vercel.com/home' className={styles.link} target="_blank" rel="noreferrer">Vercel</a>. Privacy friendly analytics with <a href='https://vercel.com/analytics' className={styles.link} target="_blank" rel="noreferrer">Vercel Analytics</a>.
             </p>    
             <p>
-                <a href="https://github.com/antonstallboerger/antonstallboerger-v2" className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">GitHub Repository</a>
+                <a href="https://github.com/antonstallboerger/antonstallboerger-v2" className={styles.link} target="_blank" rel="noreferrer">GitHub Repository</a>
             </p>
             
             <h2>Photography</h2>
@@ -37,7 +37,7 @@ export default function ColophonPage() {
 
             <h2>Mockups</h2>
             <p>
-                All mockups are by  <a href='https://shots.so/' className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">Shots</a> or <a href='https://supply.family/' className='text-zinc-500 w-fit hover:underline' target="_blank" rel="noreferrer">Supply Family</a>. 
+                All mockups are by  <a href='https://shots.so/' className={styles.link} target="_blank" rel="noreferrer">Shots</a> or <a href='https://supply.family/' className={styles.link} target="_blank" rel="noreferrer">Supply Family</a>. 
             </p>
 
             <h2>Inspiration</h2>
@@ -45,7 +45,7 @@ export default function ColophonPage() {
                 <li>
                     <a 
                         href="https://linusrogge.com/" 
-                        className='text-zinc-500 w-fit hover:underline' 
+                        className={styles.link} 
                         target="_blank" 
                         rel="noreferrer"
                     >
@@ -55,7 +55,7 @@ export default function ColophonPage() {
                 <li>
                     <a 
                         href="https://sdrn.co/" 
-                        className='text-zinc-500 w-fit hover:underline' 
+                        className={styles.link} 
                         target="_blank" 
                         rel="noreferrer"
                     >
@@ -65,7 +65,7 @@ export default function ColophonPage() {
                 <li>
                     <a 
                         href="https://www.wireframe.co/" 
-                        className='text-zinc-500 w-fit hover:underline' 
+                        className={styles.link} 
                         target="_blank" 
                         rel="noreferrer"
                     >
@@ -75,7 +75,7 @@ export default function ColophonPage() {
                 <li>
                     <a 
                         href="https://www.studiolenzing.com/" 
-                        className='text-zinc-500 w-fit hover:underline' 
+                        className={styles.link} 
                         target="_blank" 
                         rel="noreferrer"
                     >
@@ -85,7 +85,7 @@ export default function ColophonPage() {
                 <li>
                     <a 
                         href="https://goods.wtf/" 
-                        className='text-zinc-500 w-fit hover:underline' 
+                        className={styles.link} 
                         target="_blank" 
                         rel="noreferrer"
                     >
@@ -95,7 +95,7 @@ export default function ColophonPage() {
                 <li>
                     <a 
                         href="https://ollyevans.xyz/" 
-                        className='text-zinc-500 w-fit hover:underline' 
+                        className={styles.link} 
                         target="_blank" 
                         rel="noreferrer"
                     >
@@ -112,7 +112,7 @@ export default function ColophonPage() {
                 <li>
                     <a 
                         href="https://linusrogge.com/" 
-                        className='text-zinc-500 w-fit hover:underline' 
+                        className={styles.link} 
                         target="_blank" 
                         rel="noreferrer"
                     >
@@ -121,8 +121,8 @@ export default function ColophonPage() {
                 </li>
                 <li>
                     <a 
-                        href="https://designwithtech.com/" 
-                        className='text-zinc-500 w-fit hover:underline'
+                        href="https://floriankiem.com/" 
+                        className={styles.link}
                         target="_blank" 
                         rel="noreferrer"
                     >
@@ -132,7 +132,7 @@ export default function ColophonPage() {
                 <li>
                     <a 
                         href="https://www.nilseller.com/" 
-                        className='text-zinc-500 w-fit hover:underline'
+                        className={styles.link}
                         target="_blank" 
                         rel="noreferrer"
                     >
