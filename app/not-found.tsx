986a320af141a8edf45323d45b5e0091507c7b5b
Linus Rogge <hello@linusrogge.com>
2023-08-14 09:11:29 +0200

    (move) styles into module.scss

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,47 +1,57 @@
-'use client';
-
 import Link from 'next/link';
 
-export default function NotFound() {
+import styles from '../styles/notfound.module.scss'
+
+export default async function NotFound() {
     return (
         <>
-            <section className='col-span-8'>
-                <p className='font-bold'>404</p>
-                <p className=''>Page not found</p>
-            </section>
-            <section className='col-span-8 col-start-9 self-start'>
-                <p className=''>
-                    You just hit a route that doesn&#39;t exist.
+            <header className={styles.header}>
+                <h1>404</h1>
+                <p>Page not found</p>
+            </header>
+
+            <section className={styles.content}>
+                <p>
+                    You just hit a route that doesn&rsquo;t exist.
                 </p>
-                <p className='mb-8'>
+                <p>
                     This page is just as lost as you are.
                 </p>
-                <div className="flex flex-col gap-2">
-                    <Link 
-                        href='/' 
-                        className='self-start'
-                    >
-                        Home
-                    </Link>
-                    <Link 
-                        href='/about' 
-                        className='self-start'
-                    >
-                        About
-                    </Link>
-                    <Link 
-                        href='/thoughts' 
-                        className='self-start'
-                    >
-                        Thoughts
-                    </Link>
-                    <Link 
-                        href='/playground' 
-                        className='self-start'
-                    >
-                        Playground
-                    </Link>
-                </div>
+
+                <ul className={styles.pages}>
+                    <li>
+                        <Link 
+                            href='/' 
+                            className='self-start'
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            href='/about' 
+                            className='self-start'
+                        >
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            href='/thoughts' 
+                            className='self-start'
+                        >
+                            Thoughts
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            href='/playground' 
+                            className='self-start'
+                        >
+                            Playground
+                        </Link>
+                    </li>
+                </ul>
             </section>
         </>  
     );
