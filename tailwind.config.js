ffcb99521576ea53a785578d6c613e606901d207
Anton <anton.stallboerger@hfg-gmuend.de>
2023-08-26 11:59:27 +0200

    (adjust) colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
-      boxShadow: {
-        '3xl': '0 20px 50px 20px white',
+      colors: {
+        'titan': {
+          '50': '#f2f2f2',
+          '100': '#e6e6e6',
+          '200': '#cccccc',
+          '300': '#b3b3b3',
+          '400': '#999999',
+          '500': '#808080',
+          '600': '#666666',
+          '700': '#4d4d4d',
+          '800': '#333333',
+          '900': '#1a1a1a',
+          '950': '#0d0d0d',
+        },      
       },
       gridTemplateColumns: {
         // Simple 16 column grid
