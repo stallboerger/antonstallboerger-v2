0f4dc0b8e1fbd36903c77618d4ea90b32224cd25
Anton <anton.stallboerger@hfg-gmuend.de>
2023-08-26 11:57:51 +0200

    (adjust) color

--- app/page.tsx
+++ app/page.tsx
@@ -22,10 +22,10 @@ export default function Home() {
           Driven by curiosity and a love for great, high-quality and thoughtful design. Pushing the boundaries and striving to create something truly unique and meaningful. My goal is to continue improving, learning, and exploring all the different areas life has to offer.
           <br />
           <br />
-          Currently studying at the <a href="https://www.hfg-gmuend.de/en" className='text-zinc-500 hover:underline' target='_blank' rel="noreferrer">University of Design</a> in Schwäbisch Gmünd. Curating <a href="https://www.curations.tech/" className='text-zinc-500 hover:underline' target='_blank' rel="noreferrer">Curations</a> and working on different <Link href="/playground" className='text-zinc-500 hover:underline'>side projects</Link>.
+          Currently studying at the <a href="https://www.hfg-gmuend.de/en" className='text-titan-500 hover:underline' target='_blank' rel="noreferrer">University of Design</a> in Schwäbisch Gmünd. Curating <a href="https://www.curations.tech/" className='text-titan-500 hover:underline' target='_blank' rel="noreferrer">Curations</a> and working on different <Link href="/playground" className='text-titan-500 hover:underline'>side projects</Link>.
           <br />
           <br />
-          If you&rsquo;d like to <Link href="/about" className='text-zinc-500 hover:underline'>learn more about me</Link>, or discuss design, technology or life in general, feel free to <a href="mailto:hi@antonstallboerger.com" className='text-zinc-500 hover:underline'>reach out</a>. I&rsquo;m always happy to chat. 
+          If you&rsquo;d like to <Link href="/about" className='text-titan-500 hover:underline'>learn more about me</Link>, or discuss design, technology or life in general, feel free to <a href="mailto:hi@antonstallboerger.com" className='text-titan-500 hover:underline'>reach out</a>. I&rsquo;m always happy to chat. 
         </p>  
       </section>
 
@@ -41,30 +41,6 @@ export default function Home() {
             key={idx}
             />
         ))}
-        {/* <Card
-          title='Boost'
-          imageSrc='/images/main/boost.png'
-          imageAlt='Boost · Anton Stallbörger'
-          description='A app and product that helps you get a clear overview over your nutrients and gives you personalised recommendations.'
-          year='2023'
-          link='/boost'
-        />
-        <Card 
-          title='Curations'
-          imageSrc='/images/main/curations.png'
-          imageAlt='Curations · Anton Stallbörger'
-          description='A curated collection of stunning tools and website inspirations for free. Helping designers, developers, and everyone else discover new and innovative tools and ideas for their projects.'
-          year='2022 &ndash; Present'
-          link='/curations'
-        />
-        <Card
-          title='UPS App'
-          imageSrc='/images/main/ups.png'
-          imageAlt='UPS App · Anton Stallbörger'
-          description='A redesign concept of the UPS App for a faster and easier way to send and track your packages.'
-          year='2022'
-          link='/ups'
-        /> */}
       </section>
     </>
   )
