7f340111801232289560e501e50d29c98f5104b3
Anton <anton.stallboerger@hfg-gmuend.de>
2023-09-15 18:02:07 +0200

    (adjust) icon size

--- components/icons.tsx
+++ components/icons.tsx
@@ -18,8 +18,8 @@ export function LinkIcon() {
     return ( 
         <svg 
             xmlns="http://www.w3.org/2000/svg" 
-            width="20" 
-            height="20" 
+            width="16" 
+            height="16" 
             viewBox="0 0 24 24" 
             fill="none"
         >
