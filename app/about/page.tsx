1167d72fb0835e35d8c051f693d82da5ddf51786
Anton <anton.stallboerger@hfg-gmuend.de>
2023-09-16 01:13:09 +0200

    (remove) text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -90,9 +90,6 @@ export default function AboutPage() {
 			<p>
 				Pushing the boundaries of design and striving to create something truly unique and meaningful is a constant pursuit of mine. In addition to my work, I also value meaningful conversations and good music, which help to inspire and motivate me in my creative process.
 			</p>
-			<p>
-				My goal is to continue improving, learning, and exploring all the different areas life has to offer, with the ultimate goal of building a beloved home with the highly talented people at <a href="https://normcph.com/" target="_blank" className='text-black dark:text-white underline hover:decoration-titan-200 dark:hover:decoration-titan-800'>Norm Architects</a>.
-			</p>
 		</section>
 
 		<section className={styles.like}>
