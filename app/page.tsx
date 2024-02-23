e0df05df10df9dca8f6faa69adc7e084b676cab6
Anton <anton.stallboerger@hfg-gmuend.de>
2024-02-23 16:32:40 +0100

    update text

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           Driven by curiosity and a love for great, high-quality and thoughtful design. Pushing the boundaries and striving to create something truly unique and meaningful.
           <br />
           <br />
-          Currently working at <a href="https://essentry.com/en" className='text-black dark:text-white underline hover:decoration-titan-200 dark:hover:decoration-titan-800' target='_blank' rel="noreferrer">essentry</a> for an internship, living in Berlin and exploring different <Link href="/playground" className='text-black dark:text-white underline hover:decoration-titan-200 dark:hover:decoration-titan-800'>side projects</Link>.
+          Currently building icons for <a href='https://icons.saman.design' className='text-black dark:text-white underline hover:decoration-titan-200 dark:decoration-titan-800'>Saman Icons</a> and exploring different <Link href="/playground" className='text-black dark:text-white underline hover:decoration-titan-200 dark:hover:decoration-titan-800'>side projects</Link>.
         </p>
       </section>
 
