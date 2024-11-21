31868b04eecb99a51fc0ada3971edeefd190037b
Anton <anton.stallboerger@hfg-gmuend.de>
2024-11-21 20:53:26 +0100

    reindent

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -3,21 +3,21 @@ import NextImage from 'next/image'
 import { useMDXComponent } from 'next-contentlayer2/hooks'
 
 function Image(props: any) {
-    return <NextImage alt={props.alt} {...props} />;
+  return <NextImage alt={props.alt} {...props} />;
 }
 
 const components = {
-    Image
+  Image
 }
 
 interface MdxProps {
-    code: string;
+  code: string;
 }
 
 export function Mdx({ code }: MdxProps) {
-    const Component = useMDXComponent(code);
+  const Component = useMDXComponent(code);
     
-    return (
-        <Component components={{ ...components }} />
-    )
+  return (
+    <Component components={{ ...components }} />
+  )
 }
