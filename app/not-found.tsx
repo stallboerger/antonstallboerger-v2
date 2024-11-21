1f2e73b39e1195c7aa6718b00b72659134c1f572
Anton <anton.stallboerger@hfg-gmuend.de>
2024-11-21 22:42:47 +0100

    reindent

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,56 +3,56 @@ import Link from 'next/link';
 import styles from '../styles/notfound.module.css'
 
 export default async function NotFound() {
-    return (
-        <>
-            <header className={styles.header}>
-                <h1>404</h1>
-                <p>Page not found</p>
-            </header>
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>404</h1>
+        <p>Page not found</p>
+      </header>
 
-            <section className={styles.content}>
-                <p>
-                    You just hit a route that doesn&rsquo;t exist.
-                </p>
-                <p>
-                    This page is just as lost as you are.
-                </p>
+      <section className={styles.content}>
+        <p>
+          You just hit a route that doesn&rsquo;t exist.
+        </p>
+        <p>
+          This page is just as lost as you are.
+        </p>
 
-                <ul className={styles.pages}>
-                    <li>
-                        <Link 
-                            href='/' 
-                            className='self-start'
-                        >
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link 
-                            href='/about' 
-                            className='self-start'
-                        >
-                            About
-                        </Link>
-                    </li>
-                    <li>
-                        <Link 
-                            href='/thoughts' 
-                            className='self-start'
-                        >
-                            Thoughts
-                        </Link>
-                    </li>
-                    <li>
-                        <Link 
-                            href='/playground' 
-                            className='self-start'
-                        >
-                            Playground
-                        </Link>
-                    </li>
-                </ul>
-            </section>
-        </>  
-    );
+        <ul className={styles.pages}>
+          <li>
+            <Link 
+              href='/' 
+              className='self-start'
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link 
+              href='/about' 
+              className='self-start'
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link 
+              href='/thoughts' 
+              className='self-start'
+            >
+              Thoughts
+            </Link>
+          </li>
+          <li>
+            <Link 
+              href='/playground' 
+              className='self-start'
+            >
+              Playground
+            </Link>
+          </li>
+        </ul>
+      </section>
+    </>  
+  );
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -5,112 +5,112 @@ import Link from 'next/link';
 import styles from '../styles/components/footer.module.css'
 
 export default function Footer() {
-    return (
-        <footer className={`transition-fade ${styles.footer}`}>
-            <section className={styles.links}>
-                <div className={styles.connect}>
-                    <h2 className={styles.title}>Connect</h2>
-                    <ul className={styles.list}>
-                        <li>
-                            <a 
-                                href="mailto:hi@antonstallboerger.com" 
-                                className={`${styles.link}`}
-                                target="_blank" 
-                                rel="noreferrer"
-                            >
-                                Mail
-                            </a>
-                        </li>
-                        <li>
-                            <a 
-                                href="https://x.com/stallboerger" 
-                                className={`${styles.link} ${styles.twitter}`}
-                                target="_blank" 
-                                rel="noreferrer"
-                            >
-                                X.com
-                            </a>
-                        </li>
-                        <li>
-                            <a 
-                                href="https://www.cosmos.so/stallboerger" 
-                                className={styles.link} 
-                                target="_blank" 
-                                rel="noreferrer"
-                            >
-                                Cosmos
-                            </a>
-                        </li>
-                        <li>
-                            <a 
-                                href="https://read.cv/stallboerger" 
-                                className={`${styles.link} ${styles.readcv}`}
-                                target="_blank" 
-                                rel="noreferrer"
-                            >
-                                Read.cv
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+  return (
+    <footer className={`transition-fade ${styles.footer}`}>
+      <section className={styles.links}>
+        <div className={styles.connect}>
+          <h2 className={styles.title}>Connect</h2>
+          <ul className={styles.list}>
+            <li>
+              <a 
+                href="mailto:hi@antonstallboerger.com" 
+                className={`${styles.link}`}
+                target="_blank" 
+                rel="noreferrer"
+              >
+                Mail
+              </a>
+            </li>
+            <li>
+              <a 
+                href="https://x.com/stallboerger" 
+                className={`${styles.link} ${styles.twitter}`}
+                target="_blank" 
+                rel="noreferrer"
+              >
+                X.com
+              </a>
+            </li>
+            <li>
+              <a 
+                href="https://www.cosmos.so/stallboerger" 
+                className={styles.link} 
+                target="_blank" 
+                rel="noreferrer"
+              >
+                Cosmos
+              </a>
+            </li>
+            <li>
+              <a 
+                href="https://read.cv/stallboerger" 
+                className={`${styles.link} ${styles.readcv}`}
+                target="_blank" 
+                rel="noreferrer"
+              >
+                Read.cv
+              </a>
+            </li>
+          </ul>
+        </div>
 
-                <div className={styles.explore}>
-                    <h2 className={styles.title}>Explore</h2>
-                    <ul className={styles.list}>
-                        <li>
-                            <Link
-                                href='/'
-                                className={styles.link}
-                            >
-                                Home
-                            </Link>
-                        </li>
-                        <li>
-                            <Link 
-                                href='/about' 
-                                className={styles.link}
-                            >
-                                About
-                            </Link>
-                        </li>
-                        <li>
-                            <Link 
-                                href='/thoughts' 
-                                className={styles.link}
-                            >
-                                Thoughts
-                            </Link>
-                        </li>
-                        <li>
-                            <Link 
-                                href='/playground' 
-                                className='self-start'
-                            >
-                                Playground
-                            </Link>
-                        </li>
-                    </ul>
-                </div>   
+        <div className={styles.explore}>
+          <h2 className={styles.title}>Explore</h2>
+          <ul className={styles.list}>
+            <li>
+              <Link
+                href='/'
+                className={styles.link}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link 
+                href='/about' 
+                className={styles.link}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link 
+                href='/thoughts' 
+                className={styles.link}
+              >
+                Thoughts
+              </Link>
+            </li>
+            <li>
+              <Link 
+                href='/playground' 
+                className='self-start'
+              >
+                Playground
+              </Link>
+            </li>
+          </ul>
+        </div>   
 
-                <p className={styles.copyright}>&copy;{(new Date).getFullYear()} Anton Stallbörger</p>
-                <div className={styles.legals}>
-                    <Link 
-                        href='/colophon' 
-                        className={styles.link}
-                    >
-                        Colophon
-                    </Link>
-                    <Link 
-                        href='/imprint' 
-                        className={styles.link}
-                    >
-                        Imprint
-                    </Link> 
-                </div>
-            </section>
+        <p className={styles.copyright}>&copy;{(new Date).getFullYear()} Anton Stallbörger</p>
+        <div className={styles.legals}>
+          <Link 
+            href='/colophon' 
+            className={styles.link}
+          >
+            Colophon
+          </Link>
+          <Link 
+            href='/imprint' 
+            className={styles.link}
+          >
+            Imprint
+          </Link> 
+        </div>
+      </section>
 
-            <section className={styles.clock}> 
-            </section>
-        </footer>
-    );
+      <section className={styles.clock}> 
+      </section>
+    </footer>
+  );
 }
